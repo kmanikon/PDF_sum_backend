f32410259bac612abd45eaf48df04009446c8ba6
Bob Shens <kiranmanikonda123@gmail.com>
2023-02-14 23:09:22 -0800

    first commit

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 
 
-
-
 const app = express();
 dotenv.config();
 
@@ -22,10 +20,7 @@ app.use('/users', userRoutes);
 
 //CONNECTION_URL = 'mongodb://mongodb:e43FQweegr6RS6rP@mongodb-xin2-rx02-production/memories-mongo';
 
-//const CONNECTION_URL = 'mongodb://mongodb:e43FQweegr6RS6rP@mongodb-xin2-rx02-production:27017/memories-mongo';
 const CONNECTION_URL = 'mongodb+srv://kmanikon:Zamn123@cluster0.gbejc2u.mongodb.net/?retryWrites=true&w=majority';
-
-//const CONNECTION_URL = 'mongodb://mongodb:e43FQweegr6RS6rP@mongodb-xin2-rx02-production:27017/memories-mongo';
 
 const PORT = process.env.PORT || 5000;
 
--- models/postMessage.js
+++ models/postMessage.js
@@ -17,6 +17,20 @@ const postSchema = mongoose.Schema({
     },
 })
 
+/*
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+*/
+
 var PostMessage = mongoose.model('PostMessage', postSchema);
 
 export default PostMessage;
